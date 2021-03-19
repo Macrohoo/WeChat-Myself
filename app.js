1eb40afcff7e073ec185e2a1f2067fc70e9e5d60
Marhooo <105950987@qq.com>
2021-03-20 00:09:06 +0800

    0320 00:07 auth tabBar app.js

--- app.js
+++ app.js
@@ -5,15 +5,59 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    if (wx.canIUse('getUpdateManager')) {
+      const updateManager = wx.getUpdateManager()
+      updateManager.onCheckForUpdate(function(res) {
+        if (res.hasUpdate) {
+          updateManager.onUpdateReady(function() {
+            wx.showModal({
+              title: '更新提示',
+              content: '新版本已经准备好，是否重启应用？',
+              success: function(res) {
+                if (res.confirm) {
+                  updateManager.applyUpdate()
+                }
+              }
+            })
+          })
+          updateManager.onUpdateFailed(function() {
+            wx.showModal({
+              title: '已经有新版本了哟~',
+              content: '新版本已经上线啦~，请您删除当前小程序，重新搜索打开哟~'
+            })
+          })
+        }
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
+  },
+  getUserInfo: function(func) {
+    var that = this
+    if(this.globalData.userInfo) {
+      //如果这个参数func类型为函数，就执行名为func这个函数
+      typeof func == "function" && func(this.globalData.userInfo)
+    } else {
+      //调用微信登录
+      wx.login({
+        success: function(res) {
+          wx.getUserInfo({
+            success: function(res) {
+              that.globalData.userInfo = res.userInfo
+              typeof func == "function" && func(this.globalData.userInfo)
+            }
+          })
+        }
+      })
+    }
   },
   globalData: {
-    userInfo: null
+    userInfo: null,
+    openid: "",
+    isGetUserInfo: false,
+    isGetOpenid: false
   }
 })
